ba695def856e16ccdb81a5913a9d18ad85b2216e
nidzo <16163816+ndz-v@users.noreply.github.com>
2026-04-11 16:41:30 +0200

    current state

--- nvim/init.lua
+++ nvim/init.lua
@@ -151,11 +151,13 @@ else
 	vim.lsp.on_type_formatting.enable()
 	-- Enable for a specific client
 	vim.api.nvim_create_autocmd('LspAttach', {
-		callback = function(ev)
-			local client_id = ev.data.client_id
-			local client = assert(vim.lsp.get_client_by_id(client_id))
-			if client.name == 'rust-analyzer' then
-				vim.lsp.on_type_formatting.enable(true, { client_id = client_id })
+		callback = function(args)
+			vim.o.signcolumn = "yes:1"
+			local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+			if client:supports_method('textDocument/completion') then
+				vim.o.complete = 'o,.,w,b,u'
+				vim.o.completeopt = 'menu,menuone,popup,noinsert'
+				vim.lsp.completion.enable(true, client.id, args.buf)
 			end
 		end,
 	})
